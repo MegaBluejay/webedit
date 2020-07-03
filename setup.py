20964f9466cfc7980a888a5cd23b881a3a12222f
MegaBluejay <dmoiseev2011@gmail.com>
2020-07-03 21:40:37 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='webedit',
+    version='0.1',
+    packages=['webedit'],
+    url='',
+    license='',
+    author='megabluejay',
+    author_email='dmoiseev2011@gmail.com',
+    description='simple web interface for file editing'
+)
